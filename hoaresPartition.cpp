304cf5f3e61e706d940ea7297ccc827b5a074e37
Karan Dhiman <dhimankaran260@gmail.com>
2025-02-22 19:31:27 +0530

    Adds new code

---
+++ hoaresPartition.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+	int arr[] = {2,4,5,6,9,10,8,5,3,1,7};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	stable_sort(arr,arr+n);
+	for(auto i:arr)
+		cout<<i<<" ";
+	cout<<endl;
+	return 0;
+}
