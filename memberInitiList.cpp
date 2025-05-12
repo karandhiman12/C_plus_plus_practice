31f29cced525bcd3f8b6c2ccc5b4cee6d6837c3b
Karan Dhiman <dhimankaran260@gmail.com>
2025-05-12 22:47:46 +0530

    member initialization list in C++ is used to initialize class members directly when an object is constructed, instead of assigning values within the constructor body.

---
+++ memberInitiList.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class Example {
+public:
+    // Default constructor: prints a message when an Example object is created
+    Example() {
+        std::cout << "CREATED ENTITY" << std::endl;
+    }
+
+    // Parameterized constructor: prints a message showing the passed integer value
+    Example(int x) {
+        std::cout << "CREATED ENTITY WITH: " << x << std::endl;
+    }
+};
+
+class Entity {
+private:
+    std::string m_Name;  // Name of the entity
+    int m_Score;         // Score associated with the entity
+    Example m_Example;   // Example object as a member variable
+
+public:
+    // Default constructor: initializes member variables using an initializer list
+    Entity()
+        : m_Name("Unknown"), m_Score(87), m_Example(56) // Creates an Example instance with value 56
+    {
+        // Alternative way of assigning values after initialization (commented out)
+        // m_Example = Example(56);
+    }
+
+    // Parameterized constructor: initializes entity name while leaving other attributes default
+    Entity(const std::string& name) : m_Name(name) {
+        // Alternative way of assigning values after initialization (commented out)
+        // m_Name = name;
+    }
+
+    // Getter function to retrieve the entity's name
+    const std::string GetName() const {
+        return m_Name;
+    }
+};
+
+int main() {
+    // Creating an Entity object using the default constructor
+    Entity e0;
+
+    return 0;
+}
