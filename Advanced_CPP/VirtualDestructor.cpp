0fdf61a42508f46f9cd9091c2fb75f62d7bcba9d
Karan Dhiman <dhimankaran260@gmail.com>
2025-07-02 22:35:59 +0530

    virtual destructors

---
+++ Advanced_CPP/VirtualDestructor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Base
+{
+public:
+    Base() { std::cout<<"Base Constructor\n"; }
+    ~Base() { std::cout<<"Base Destructor\n"; }
+    
+};
+
+class Derived: public Base
+{
+public:
+    Derived() { std::cout<<"Derived Constructor\n"; }
+    ~Derived() { std::cout<<"Derived Destructor\n"; }
+    
+};
+
+int main() {
+    Base* base = new Base();
+    delete base;
+    std::cout<<"-------------\n";
+    Derived* derived = new Derived();
+    delete derived;
+    return 0;
+}
