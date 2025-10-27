1a36010b138f5f65c3c19853f4bc0eef60250b4a
Karan Dhiman <dhimankaran260@gmail.com>
2025-10-27 23:03:52 +0530

    lb.cpp

---
+++ Second/lb.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void swapCharArray(char arr[], int n) {
+    int l = 0, h = n - 1;
+    while(l<h){
+        swap(arr[l], arr[h]);
+        l++;h--;
+    }
+}
+bool checkPalindrome(char ch[], int n) {
+    int l=0,h=n-1;
+    while(l<=h) {
+        if(ch[l] != ch[h])
+            return false;
+        l++;h--;
+    }
+    return true;
+}
+int main()
+{
+    char name[] = "Malayalam";
+    cout<<boolalpha;
+    int i = 0;
+    while(name[i] != '\0') {
+         i++;
+    }
+    swapCharArray(name, i);
+    cout<<name<<endl;
+    cout<<checkPalindrome(name,i);
+    return 0;
+}
