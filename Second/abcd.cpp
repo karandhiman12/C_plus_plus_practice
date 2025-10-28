9226d1fdfd14ee531a84422d3d5cd2bb336d129f
Karan Dhiman <dhimankaran260@gmail.com>
2025-10-28 23:20:24 +0530

    abcd.cpp

---
+++ Second/abcd.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    Base() {
+        cout<<"Base Constructor\n";
+    }
+    virtual ~Base() {
+        cout<<"Base Destructor\n";
+    }
+    virtual void memFunction() {
+        cout<<"Base:: member function\n";
+    }
+};
+
+class Derived : public Base {
+public:
+    Derived() {
+        cout<<"Derived Constructor\n";
+    }
+    ~Derived() {
+        cout<<"Derived Destructor\n";
+    }
+    void memFunction() override{
+        cout<<"Derived:: member function\n";
+    }
+};
+
+int main(){
+    Base *instance = new Derived;
+    instance->memFunction();
+    delete instance;
+    double a = 5.25;
+    int b = static_cast<int>(a) + .29;
+    cout<<b;
+    return 0;
+}
