bc55cd41f147f85e6333e82051c2f763487e52be
Karan Dhiman <dhimankaran260@gmail.com>
2025-11-24 22:17:16 +0530

    binary string ti dna encoding

---
+++ Second/binary2dna.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        string ans;
+        for(int i=0;i<n-1;i+=2) {
+            if(s[i]=='0' && s[i+1] == '0') {
+                ans.push_back('A');
+            } else if(s[i]=='0' && s[i+1]=='1') {
+                ans.push_back('T');
+            } else if(s[i]=='1' && s[i+1] == '0') {
+                ans.push_back('C');
+            } else if(s[i]=='1' && s[i+1]=='1'){
+                ans.push_back('G');
+            }
+        }
+        cout<<ans<<endl;
+    }
+
+}
