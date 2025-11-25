f49c0c0dc3f8902decf2256bf52191587a70f336
Karan Dhiman <dhimankaran260@gmail.com>
2025-11-25 21:20:13 +0530

    changes

--- Second/binary2dna.cpp
+++ Second/binary2dna.cpp
@@ -10,6 +10,7 @@ int main() {
         cin>>n;
         string s;
         cin>>s;
+        char mm[] = {'A','T','C','G'};
         string ans;
         for(int i=0;i<n-1;i+=2) {
             if(s[i]=='0' && s[i+1] == '0') {
